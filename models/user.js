43e8817710c5b66df3efcc3430e52dac92d5494f
Elmejorantho <elmejoranthony233@hotmail.com>
2023-01-10 13:40:07 -0400

    Agregada otra linea

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 const mongoose = require ('mongoose');
 
+mongoose.set("strictQuery", false);
+
+mongoose.connect(process.env.MONGO_URI);
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
